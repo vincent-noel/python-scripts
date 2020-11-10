16011869ea36fb945c15e8637e0717b755bc8763
Randy Heiland <heiland@indiana.edu>
2020-11-10 16:02:48 -0500

    cleanup

--- analysis/covid19/plot_immune_cells.py
+++ analysis/covid19/plot_immune_cells.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 argc = len(sys.argv)-1
 print("# args=",argc)
 
-data_dir = 'output'
+#data_dir = 'output'
 if (argc < 1):
 #  data_dir = int(sys.argv[kdx])
   print("Usage: provide output subdir")
@@ -16,21 +16,15 @@ if (argc < 1):
 
 kdx = 1
 data_dir = sys.argv[kdx]
-
 print('data_dir = ',data_dir)
-
 os.chdir(data_dir)
-#xml_files = glob.glob('output/output*.xml')
 xml_files = glob.glob('output*.xml')
 os.chdir('..')
 xml_files.sort()
 #print('xml_files = ',xml_files)
-n = len(xml_files)
 
 ds_count = len(xml_files)
 print("----- ds_count = ",ds_count)
-#mcds = [pyMCDS(xml_files[i], '.') for i in range(ds_count)]
-#mcds = [pyMCDS_cells(xml_files[i], '.') for i in range(ds_count)]
 mcds = [pyMCDS_cells(xml_files[i], data_dir) for i in range(ds_count)]
 
 tval = np.linspace(0, mcds[-1].get_time(), ds_count)
@@ -57,11 +51,6 @@ yval8 = np.array( [(np.count_nonzero((mcds[idx].data['discrete_cells']['cell_typ
 # count Fibroblasts
 yval9 = np.array( [(np.count_nonzero((mcds[idx].data['discrete_cells']['cell_type'] == 8) & (mcds[idx].data['discrete_cells']['cycle_model'] < 100.) == True)) for idx in range(ds_count)] )
 
-
-# #plt.plot(t,num_mac,'-o',)
-#plt.plot(t,num_mac, t,num_cd8, t,num_neut)
-#plt.plot(tval,yval4, tval,yval5, tval,yval6, tval,yval7, tval,yval8, tval,yval9 )
-
 plt.plot(tval, yval4, label='Mac', linewidth=1, color='lime')
 plt.plot(tval, yval5, linestyle='dashed', label='Neut', linewidth=1, color='cyan')
 plt.plot(tval, yval6, label='CD8', linewidth=1, color='red')
@@ -71,7 +60,6 @@ plt.plot(tval, yval9, linestyle='dashed',  label='Fib', linewidth=1, color='oran
 
 #plt.legend(loc='center left', prop={'size': 15})
 plt.legend(loc='top left', prop={'size': 10})
-
 
 plt.title(data_dir)
 plt.savefig(data_dir + '.png')
--- analysis/covid19/plot_viral_load.py
+++ analysis/covid19/plot_viral_load.py
@@ -8,29 +8,24 @@ import matplotlib.pyplot as plt
 argc = len(sys.argv)-1
 print("# args=",argc)
 
-data_dir = 'output'
 if (argc < 1):
 #  data_dir = int(sys.argv[kdx])
   print("Usage: provide output subdir")
   sys.exit(-1)
 
+#data_dir = 'output'
 kdx = 1
 data_dir = sys.argv[kdx]
-
 print('data_dir = ',data_dir)
-
 os.chdir(data_dir)
 #xml_files = glob.glob('output/output*.xml')
 xml_files = glob.glob('output*.xml')
 os.chdir('..')
 xml_files.sort()
 #print('xml_files = ',xml_files)
-n = len(xml_files)
 
 ds_count = len(xml_files)
 print("----- ds_count = ",ds_count)
-#mcds = [pyMCDS(xml_files[i], '.') for i in range(ds_count)]
-#mcds = [pyMCDS_cells(xml_files[i], '.') for i in range(ds_count)]
 mcds = [pyMCDS_cells(xml_files[i], data_dir) for i in range(ds_count)]
 
 tval = np.linspace(0, mcds[-1].get_time(), ds_count)
@@ -39,21 +34,6 @@ print('tval= ',tval)
 y_load = np.array( [np.floor(mcds[idx].data['discrete_cells']['assembled_virion']).sum()  for idx in range(ds_count)] ).astype(int)
 print(y_load)
 
-# idx = 0
-# for f in xml_files:
-#     mcds = pyMCDS_cells(f, data_dir)
-#     cell_type = mcds.data['discrete_cells']['cell_type']
-#     cd8 = np.where(cell_type == 3.0)
-#     mac = np.where(cell_type == 4.0)
-#     neut = np.where(cell_type == 5.0)
-#     t[idx] = mcds.get_time()
-#     num_mac[idx] = len(mac[0])
-#     num_cd8[idx] = len(cd8[0])
-#     num_neut[idx] = len(neut[0])
-#     idx += 1
-
-# #plt.plot(t,num_mac,'-o',)
-#plt.plot(t,num_mac, t,num_cd8, t,num_neut)
 plt.plot(tval,y_load)
 plt.title(data_dir)
 plt.savefig(data_dir + '.png')
